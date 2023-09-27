5836d73cf660629a0e356630b8dea392174d5f91
leanne-b <81729265+leanne-b@users.noreply.github.com>
2023-09-27 15:16:39 +1000

    save feature ranking (#20)
    * save feature ranking
    
    * Adding none option to datawrangling
    
    ---------
    
    Co-authored-by: Jennie Kwok <jenniekwok11@gmail.com>

--- src/components/FeatureBarChart.tsx
+++ src/components/FeatureBarChart.tsx
@@ -2,19 +2,35 @@
 import React from "react"; // Make sure to import React
 import { Bar } from "react-chartjs-2";
 import type { ChartData, ChartOptions } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 interface BarChartProps {
     data: ChartData<'bar'>;
+    title?: boolean
   }
 
-  const BarChart: React.FC<BarChartProps> = ({ data }) => {
+  const BarChart: React.FC<BarChartProps> = ({ data, title }) => {
     return (
-      <div className="chart-container">
-        <h2 style={{ textAlign: "center" }}>Feature Importance</h2>
-        <Bar data={data} options={{
+      <div className="chart-container" style={{width: "100%", marginTop: "1rem", marginBottom: "1rem"}}>
+       {title !== false && <h2 style={{ textAlign: "center" }}>Feature Importance</h2>}
+        <Bar data={data} 
+        plugins={[ChartDataLabels]}
+        options={{
           plugins: {
+            datalabels: {
+              align: 'end',
+              anchor: 'end'
+            },
             title: {
               display: true,
+            },
+            legend: {
+              display: false,
+              title: {
+                display: true,
+                text: "Features"
+
+              }
             }
           }
         }} />
